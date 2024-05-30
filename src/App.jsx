1f0e7612bb637cee1432e469aab7aecc292263c1
popaleonchika2000 <sorokinan269@gmail.com>
2024-05-30 15:15:15 +0300

    Добавили стили на сайт для шапки

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 const App = () => {
-  return <Navbar/>
+  return (
+    <Home/>
+  )
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,35 +1,83 @@
 import React from 'react'
-import '../App.css'
+import styled from "styled-components"
+import SearchIcon from '@mui/icons-material/Search';
+import { StyledBadge } from '@mui/material';
+
+const Container = styled.div`
+    height: 174px;
+    width: 1440px !important;
+    background-color: #ffdfb7;
+`
+const Headd = styled.ul`
+    height: 60px;
+    display: flex;
+    list-style: none;
+    margin: 0;  
+    padding: 20px;
+    font-size: 12px;
+`
+const Left = styled.div`
+    flex: 1;
+    text-align: left;
+    width: 300px;
+    align-items: center;
+`
+const Center = styled.div`
+    flex: 3;    
+    text-align: left;
+    align-items: right;
+    display: flex;
+`
+const Right = styled.div`
+    flex: 1;
+    text-align: right;
+    align-items: right;
+    display: flex;
+`
+const SearchContainer = styled.div`
+    border: 1px solid orange;
+    align-items: center;
+    display: flex;
+    padding: 5px;
+    width: fit-content;
+    
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-right: 30px;
+`
+const Input = styled.input`
+    border: none;
+`
+
 const Navbar = () => {
     return (
-        <div class="boss">
-            <div class='head'>
-                <nav>
-                    <ul>
-                        <li><a href="#">О нас</a></li>
-                        <li><a href="#">Доставка и оплата</a></li>
-                        <li><a href="#">Вопросы и ответы</a></li>
-                        <li><a href="#">Отзывы</a></li>
-                        <li><a href="#">Статьи</a></li>
-                        <li><a href="#">Контакты</a></li>
-                        <li><a href="#">Еще</a></li>
-                        <li><a href="#">+7 (3452) 59-49-45</a></li>
-                        <li><a href="#">Заказать звонок</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="search_block">
-                <form>
-                    <input type="search" name="text" class="search" placeholder="Поиск товаров" />
-                    <input type="submit" name="submit" class="submit" value="" />
-                </form>
-            </div>
-            <div class="account">
-                <form>
-                    <input type="text" name="text" class="text" placeholder="Ваш аккаунт" />
-                </form>
-            </div>
-        </div>
+        <Container>
+            <Headd>
+                <Left>
+                    <p>Новый Уренгой</p>
+                    <img src="../pes.svg" alt="" />
+                </Left>
+                <Center>
+                    <MenuItem> О нас</MenuItem>
+                    <MenuItem> Доставка и оплата</MenuItem>
+                    <MenuItem>Вопросы и ответы</MenuItem>
+                    <MenuItem>Отзывы</MenuItem>
+                    <MenuItem>Статьи</MenuItem>
+                    <MenuItem>Контакты</MenuItem>
+                    <br />
+                    <SearchContainer>
+                        <SearchIcon style={{fontSize: 16 }} />
+                        <Input></Input>
+                    </SearchContainer>
+                </Center>
+                <Right>
+                    <MenuItem>Регистрация</MenuItem>
+                    <MenuItem>Войти</MenuItem>
+                </Right>
+            </Headd>
+        </Container>
     )
 }
 
